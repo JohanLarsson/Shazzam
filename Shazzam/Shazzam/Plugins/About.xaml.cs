46b7b0c7a416b1a850d9c37c46ca81b99683aa82
SND\WaltRitscher_cp <SND\WaltRitscher_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-03-09 19:41:28 +0000

    Fixed version number for About page
    git-svn-id: https://shazzam.svn.codeplex.com/svn@40926 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Shazzam/Shazzam/Plugins/About.xaml.cs
+++ Shazzam/Shazzam/Plugins/About.xaml.cs
@@ -23,6 +23,7 @@ namespace Shazzam.Plugins
 			InitializeComponent();
 			this.AddHandler(Hyperlink.RequestNavigateEvent, new RoutedEventHandler(this.HandleRequestNavigate), false);
 			versionText.Text = GetVersionNumber();
+			versionRun.Text = String.Format("v{0}", GetShortVersionNumber());
 		}
 
 		void HandleRequestNavigate(object sender, RoutedEventArgs e)
@@ -60,6 +61,35 @@ namespace Shazzam.Plugins
 			return ourVersion;
 
 			        }
+		private string GetShortVersionNumber()
+		{
+			System.Reflection.Assembly _assemblyInfo = System.Reflection.Assembly.GetExecutingAssembly();
+
+			string ourVersion = string.Empty;
+
+			//if running the deployed application, you can get the version
+			//  from the ApplicationDeployment information. If you try
+			//  to access this when you are running in Visual Studio, it will not work.
+			if
+			(System.Deployment.Application.ApplicationDeployment.IsNetworkDeployed)
+			{
+			//	ourVersion = ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString();
+				ourVersion = String.Format("{0}.{1}",
+																			ApplicationDeployment.CurrentDeployment.CurrentVersion.Major.ToString(),
+																			ApplicationDeployment.CurrentDeployment.CurrentVersion.Minor.ToString());
+			}
+			else
+			{
+				if (_assemblyInfo != null)
+				{
+					ourVersion = String.Format("{0}.{1}",
+						                          _assemblyInfo.GetName().Version.Major.ToString(),
+																			_assemblyInfo.GetName().Version.Minor.ToString());
+				}
+			}
+			return ourVersion;
+
+		}
 		}
 	}
 
