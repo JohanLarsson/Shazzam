6aaf7e60c6f614dbc4efc24c46455d2ad1e253d3
SND\WaltRitscher_cp <SND\WaltRitscher_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-05-13 21:38:35 +0000

    Add TexturePicker
    git-svn-id: https://shazzam.svn.codeplex.com/svn@49196 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Shazzam/Shazzam/Controls/TexturePicker.xaml.cs
+++ Shazzam/Shazzam/Controls/TexturePicker.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Resources;
+using System.Linq;
 
 namespace Shazzam.Controls
 {
@@ -13,8 +18,7 @@ namespace Shazzam.Controls
 		static Dictionary<int, ImageBrush> _images = new Dictionary<int, ImageBrush>();
 		ShaderModelConstantRegister _register = null;
 		public TexturePicker(ShaderModelConstantRegister register)
-
-		{ 
+		{
 			InitializeComponent();
 			_register = register;
 			//this.Loaded += new RoutedEventHandler(TexturePicker_Loaded);
@@ -25,8 +29,47 @@ namespace Shazzam.Controls
 			{
 				Value = new ImageBrush(image1.Source);
 			}
+			SetupTextures();
 		}
+		public const string AssemblyPrefix = "images/texturemaps/";
+		private void SetupTextures()
+		{
+			string path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			path += "\\Images\\TextureMaps";
+			foreach (var file in System.IO.Directory.GetFiles(path))
+			{
+				cboIncludedTextures.Items.Add(System.IO.Path.GetFileName(file));
+			}
+		}
+		private void SetupTextures2()
+		{
 
+			var rm = new ResourceManager(Assembly.GetExecutingAssembly().GetName().Name + ".g", Assembly.GetExecutingAssembly());
+
+			ResourceSet rs = rm.GetResourceSet(CultureInfo.CurrentCulture, true, true);
+			var maps = new List<string>();
+			foreach (System.Collections.DictionaryEntry r in rs)
+			{
+				var name = r.Key.ToString();
+				if (name.StartsWith(AssemblyPrefix))
+				{
+					maps.Add(name.Remove(0, AssemblyPrefix.Length));
+				}
+			}
+
+			maps.Sort();
+			foreach (var entry in maps)
+			{
+				//	var name = entry.Key.ToString();
+				//if (name.StartsWith(AssemblyPrefix))
+				//{
+				//  cboIncludedTextures.Items.Add();
+				//}
+				cboIncludedTextures.Items.Add(entry);
+
+			}
+
+		}
 		void TexturePicker_Loaded(object sender, RoutedEventArgs e)
 		{
 			//ImageBrush result;
@@ -81,7 +124,7 @@ namespace Shazzam.Controls
 		{
 			ImageBrush brush = (ImageBrush)e.NewValue;
 			_images[_register.RegisterNumber] = brush;
-			image1.Source = image2.Source= brush.ImageSource;
+			image1.Source = image2.Source = brush.ImageSource;
 		}
 
 		#endregion
@@ -104,12 +147,65 @@ namespace Shazzam.Controls
 
 		private void UserControl_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
 		{
-		//	this.Height = 180;
+			//	this.Height = 180;
 		}
 
 		private void UserControl_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
 		{
-		//	this.Height = 70;
+			//	this.Height = 70;
+		}
+
+		private void cboIncludedTextures_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			//switch (cboIncludedTextures.SelectedItem.ToString())
+			//{
+			//case "Clouds":
+			//  bi = new BitmapImage (new Uri("/Shazzam;component/Images/Textures/clouds.png",UriKind.RelativeOrAbsolute));
+			//  image1.Source = bi;
+			//  break;
+			//case "Cactus":
+			//  bi = new BitmapImage(new Uri("/Shazzam;component/Images/cactus.jpg", UriKind.RelativeOrAbsolute));
+			//  image1.Source = bi;
+			//  break;
+			//case "Sweep":
+			//  bi = new BitmapImage(new Uri("/Shazzam;component/Images/Textures/colorrange.png", UriKind.RelativeOrAbsolute));
+			//  image1.Source = bi;
+			//  break;
+			//case "Blocks":
+			//  bi = new BitmapImage(new Uri("/Shazzam;component/Images/Textures/blocks.jpg", UriKind.RelativeOrAbsolute));
+			//  image1.Source = bi;
+			//  break;
+			//case "GradientBlocks":
+			//  bi = new BitmapImage(new Uri("/Shazzam;component/Images/Textures/gradientblocks.png", UriKind.RelativeOrAbsolute));
+			//  image1.Source = bi;
+			//  break;
+			//case "Snowflake":
+			//  bi = new BitmapImage(new Uri("/Shazzam;component/Images/Textures/koch_snowflake.jpg", UriKind.RelativeOrAbsolute));
+			//  image1.Source = bi;
+			//  break;
+			//  case "RippledGlass":
+			//  bi = new BitmapImage(new Uri("/Shazzam;component/Images/Textures/rippled_glass.png", UriKind.RelativeOrAbsolute));
+			//  image1.Source = bi;
+			//  break;
+			//default:
+			//  break;
+			//}
+			BitmapImage bi = new BitmapImage(new Uri("/Shazzam;component/Images/cactus.jpg", UriKind.RelativeOrAbsolute)); ;
+
+			string uriLocation = string.Format("images/texturemaps/{0}", cboIncludedTextures.SelectedItem.ToString());
+			//bi = new BitmapImage(new Uri(uriLocation,UriKind.RelativeOrAbsolute));
+			//ImageBrush brush = new ImageBrush(bi);
+			//Value = brush;
+			//Shazzam.Images.Textures.clouds.png
+
+			Uri resourceUri = new Uri(uriLocation, UriKind.Relative);
+
+			StreamResourceInfo streamInfo = Application.GetContentStream(resourceUri);
+
+			BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+			ImageBrush brush = new ImageBrush(temp);
+			Value = brush;
+
 		}
 	}
 }
