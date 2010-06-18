bc11f1c2f602927ebbeb5ac9dafddb3f559c44d9
SND\WaltRitscher_cp <SND\WaltRitscher_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2010-06-18 03:45:52 +0000

    Remove setting for FXC compiler, no need for this anymore, because we are using the D3Dcompiler
    git-svn-id: https://shazzam.svn.codeplex.com/svn@54885 2327b42d-5241-43d6-9e2a-de5ac946f064

--- Shazzam/Shazzam/Plugins/SettingsPlugin.xaml.cs
+++ Shazzam/Shazzam/Plugins/SettingsPlugin.xaml.cs
@@ -1,61 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
+namespace Shazzam.Plugins
+{
 
-namespace Shazzam.Plugins {
-	/// <summary>
-	/// Interaction logic for SettingsPlugin.xaml
-	/// </summary>
-	public partial class SettingsPlugin : UserControl {
-		public SettingsPlugin() {
-			InitializeComponent();
-			this.Loaded += new RoutedEventHandler(SettingsPlugin_Loaded);
-		}
+  public partial class SettingsPlugin : UserControl
+  {
+    public SettingsPlugin()
+    {
+      InitializeComponent();
+      this.Loaded += new RoutedEventHandler(SettingsPlugin_Loaded);
+    }
 
-		void SettingsPlugin_Loaded(object sender, RoutedEventArgs e)
-		{
-			//if (RenderCapability.IsPixelShaderVersionSupported(3, 0))
-			//{
-			//  notAvailable.Visibility = Visibility.Collapsed;
+    void SettingsPlugin_Loaded(object sender, RoutedEventArgs e)
+    {
+      if (RenderCapability.IsPixelShaderVersionSupported(3, 0))
+      {
+        notAvailable.Visibility = Visibility.Collapsed;
 
-			//}
-			//else
-			//{
-			//  notAvailable.Visibility = Visibility.Visible;
-			//  notAvailable.ToolTip = "PS_3 is not supported on your video card.";
-			//}
-		}
-
-		private void ChooseFxcLocation_Click(object sender, RoutedEventArgs e) {
-			var ofd = new Microsoft.Win32.OpenFileDialog();
-			ofd.Filter = "DirectX compiler|*.exe|All Files|*.*";
-
-			if (Properties.Settings.Default.DirectX_FxcPath != string.Empty)
-			{
-				string fxcPath = Environment.ExpandEnvironmentVariables(Properties.Settings.Default.DirectX_FxcPath);
-				ofd.InitialDirectory = System.IO.Path.GetDirectoryName(fxcPath);
-			}
-			if (ofd.ShowDialog() == true)
-			{
-				Properties.Settings.Default.DirectX_FxcPath = ofd.FileName;
-				Properties.Settings.Default.Save();
-			}
-		}
-
-		private void RestoreDefaultFxcLocation_Click(object sender, RoutedEventArgs e) {
-			Properties.Settings.Default.DirectX_FxcPath = @"%DXSDK_DIR%\Utilities\bin\x86\fxc.exe";
-			Properties.Settings.Default.Save();
-		}
-	}
+      }
+      else
+      {
+        notAvailable.Visibility = Visibility.Visible;
+        notAvailable.ToolTip = "PS_3 is not supported on your video card.";
+      }
+    }
+  }
 }
