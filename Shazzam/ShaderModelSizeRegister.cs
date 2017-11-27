8ac036c19df26a19cc58cd22addac04b7d7f1c6d
JohanLarsson <johan_gavle1@hotmail.com>
2017-11-27 19:25:12 +0100

    Reset button.

--- Shazzam/ShaderModelSizeRegister.cs
+++ Shazzam/ShaderModelSizeRegister.cs
@@ -14,8 +14,8 @@
                 min,
                 max,
                 defaultValue,
-                x => new RegisterProperty<Size>("Width", x, p => p.Width, (p, d) => new Size(d, p.Height)),
-                x => new RegisterProperty<Size>("Height", x, p => p.Height, (p, d) => new Size(p.Width, d)))
+                x => new RegisterProperty<Size>("W", x, p => p.Width, (p, d) => new Size(d, p.Height)),
+                x => new RegisterProperty<Size>("H", x, p => p.Height, (p, d) => new Size(p.Width, d)))
         {
         }
     }
