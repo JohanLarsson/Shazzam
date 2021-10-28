0caa91bbc18774e911303b79278bbdfaec0182e9
JohanLarsson <johan_gavle1@hotmail.com>
2021-10-28 09:17:28 +0200

    Fix warnings.

--- Shazzam/Properties/Settings.cs
+++ Shazzam/Properties/Settings.cs
@@ -5,16 +5,11 @@
     // The PropertyChanged event is raised after a setting's value is changed.
     // The SettingsLoaded event is raised after the setting values are loaded.
     // The SettingsSaving event is raised before the setting values are saved.
-    public sealed partial class Settings
+    internal sealed partial class Settings
     {
-        public Settings()
+        internal Settings()
         {
-            this.PropertyChanged += SettingsPropertyChanged;
-        }
-
-        private static void SettingsPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            Default.Save();
+            this.PropertyChanged += (sender, e) => Default.Save();
         }
     }
 }
